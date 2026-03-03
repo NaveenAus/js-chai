a33c917e7bea2e385616f17849e808d4381428c4
NaveenAus <naymer209@gmail.com>
2026-03-03 13:35:01 +0000

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,50 @@
+//console.log(isNaN(true))
+// so we cant embed variables ie ${name} in "" and ' ' but we can do it in ``
+//let name="Naveen"
+//let msg=`my name is ${name}`
+//console.log(msg)
+//numbers
+// let x=5       // integer
+// let y=4.5     // decimals
+// let z=-1      // negative 
+// console.log(x+y+z) // gives out put as 8.5
+
+
+// string we have to options ie " " and ' '
+// so we cant embed variables ie ${name} in "" and ' ' but we can do it in ``
+//let name="Naveen"
+//let msg=`my name is ${name}`
+//console.log(msg)
+
+
+
+// booleaan 
+// in boolean we have two options ie true or false
+// let login=true
+// let haspermission=false
+// if(login){
+//     console.log("welcome!")
+// }
+// if(haspermission){
+//     console.log("yo")
+// }
+
+
+//undefined  =A variable declared but not yet given a value.
+// let score
+// console.log(score)
+
+//null=An intentionally empty value. You set it on purpose.
+// let userId=null
+// console.log(userId)
+
+//Bigint =For numbers too large for the regular Number type to handle accurately.
+// let big=249294942484209094924938584688478n// so for really big numbers like this just add n at the end
+// console.log(big+1n)
+
+//symbol=Creates a completely unique value, even if two symbols look the same. Used in advanced code.
+// let name=Symbol("naveen")
+// let name2=Symbol("naveen")
+// console.log(name===name2)
+
+
